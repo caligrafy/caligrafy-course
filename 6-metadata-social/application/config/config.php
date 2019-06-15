12246523fcae64b701b3c211bbf9e49d1bfba1d5
Dory Azar <dory.azar@gmail.com>
2019-06-14 23:55:51 -0400

    Adding part 3 lectures

---
+++ 6-metadata-social/application/config/config.php
@@ -0,0 +1,23 @@
+<?php 
+
+/**
+ * config.php is used for any additional configurations needed for the application
+ * @author Dory A.Azar
+ * @copyright 2019
+ * @version 1.0
+ *
+*/
+
+metadata(array(
+  'site_name' => 'Showcase',
+  'google-site-verification' => '',
+  'site_url' => 'http://showcase.com',
+  'title' => 'My Portfolio',
+  'description' => 'this is a description of my website',
+  'creator' => 'Your name',
+  'image' => 'http://url-to-the-site-banner',
+  'amount' => '0.00',
+  'currency' => 'USD',
+  'shipping_cost' => '0.00',
+  'availability' => 'in stock'  
+));
